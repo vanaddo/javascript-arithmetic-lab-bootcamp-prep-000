0d1f0ad8f35665674ee2f76d1c665557329e5a82
vanaddo@gmail.com <vanaddo@gmail.com>
2018-08-15 03:49:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ function dec(n)
 
 function makeInt(n){
   
-  return parseInt('n', 10)
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n){
   
-  return parseFloat(n)
+  return parseFloat(n);
 }
 
 
