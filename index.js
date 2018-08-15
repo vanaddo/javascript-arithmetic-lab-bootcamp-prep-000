e13542a372ea0ddecb8b5971bde77260fc56cf95
vanaddo@gmail.com <vanaddo@gmail.com>
2018-08-15 03:41:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function subtract(a,b)
   var result = a - b;
   return result;
 }
+
+function multiply(a,b)
+{
+  var result = a - b;
+  return result;
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
